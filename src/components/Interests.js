785485aa98615f49a915a459a541698f4f690135
Oksana Oberemok <Pylypenkok@gmail.com>
2022-09-28 20:11:00 +0800

    fixed section className

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,6 +1,6 @@
 const Interests = () => {
   return (
-    <section className="section">
+    <section className="section pb-25">
       <h2 className="section-title">Interests</h2>
       <p className="section-text">
         Youtube vloger, coffee fanatic, travel geek, best motorbike driver
