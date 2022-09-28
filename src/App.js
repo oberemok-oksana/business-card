1b0e2422a02bb30f90d85853ee179b50c63b5736
Oksana Oberemok <Pylypenkok@gmail.com>
2022-09-28 20:11:36 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Info from "./components/Info";
 import Interests from "./components/Interests";
 
@@ -9,6 +10,7 @@ function App() {
       <Info />
       <About />
       <Interests />
+      <Footer />
     </div>
   );
 }
