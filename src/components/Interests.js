68c1a90edbe5b8f1132e5c88b083008e9926c57f
Oksana Oberemok <Pylypenkok@gmail.com>
2022-09-28 17:49:02 +0800

    implement Interests component

---
+++ src/components/Interests.js
@@ -0,0 +1,12 @@
+const Interests = () => {
+  return (
+    <section className="section">
+      <h2 className="section-title">Interests</h2>
+      <p className="section-text">
+        Youtube vloger, coffee fanatic, travel geek, best motorbike driver
+      </p>
+    </section>
+  );
+};
+
+export default Interests;
