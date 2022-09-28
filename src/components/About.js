4c4ea56131c046f04afc2fc37aab85c41597248b
Oksana Oberemok <Pylypenkok@gmail.com>
2022-09-28 17:48:51 +0800

    implement About component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <section className="section">
+      <h2 className="section-title">About</h2>
+      <p className="section-text">
+        I have always been fascinated by innovations because they can improve
+        and facilitate the life of other people and my life as well. Programming
+        is exactly the place of innovations therefore I want to be a part of it.
+        I believe I could make a great contribution of my skills in solving
+        challenging problems and reaching goals.
+      </p>
+    </section>
+  );
+};
+
+export default About;
